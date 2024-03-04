08ed790d13ad196cd4ea6ec6f831a188b524b36e
jjcx <112969641+jjcxdev@users.noreply.github.com>
2024-03-04 10:55:41 -0500

    Auto-Updated configs backup

--- nvim/init.lua
+++ nvim/init.lua
@@ -90,6 +90,9 @@ P.S. You can delete this when you're done too. It's your config now! :)
 vim.g.mapleader = ' '
 vim.g.maplocalleader = ' '
 
+-- Remove ~ from left
+vim.opt.fillchars = { eob = ' ' }
+
 -- [[ Setting options ]]
 -- See `:help vim.opt`
 -- NOTE: You can change these options as you wish!
